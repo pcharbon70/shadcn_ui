3ac08ea923b22b348482366661d609777f99c961
Pascal Charbonneau <pcharbon70@gmail.com>
2026-09-01 11:17:26 -0400

    fix(demo): remove invalid runtime regex options

--- demo/config/runtime.exs
+++ demo/config/runtime.exs
@@ -30,10 +30,10 @@ if config_env() == :dev do
       web_console_logger: true,
       patterns: [
         # Static assets, except user uploads
-        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
         # Router, Controllers, LiveViews and LiveComponents
-        ~r"lib/shadcn_ui_demo_web/router\.ex$"E,
-        ~r"lib/shadcn_ui_demo_web/(controllers|live|components)/.*\.(ex|heex)$"E
+        ~r"lib/shadcn_ui_demo_web/router\.ex$",
+        ~r"lib/shadcn_ui_demo_web/(controllers|live|components)/.*\.(ex|heex)$"
       ]
     ]
 end
