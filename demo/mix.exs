8654f6a4500ce210682d7cae7453553d878a714c
Pascal Charbonneau <pcharbon70@gmail.com>
2026-09-03 13:01:15 -0400

    Align demo release with ShadcnUI 1.0.0

--- demo/mix.exs
+++ demo/mix.exs
@@ -4,7 +4,7 @@ defmodule ShadcnUIDemo.MixProject do
   def project do
     [
       app: :shadcn_ui_demo,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
